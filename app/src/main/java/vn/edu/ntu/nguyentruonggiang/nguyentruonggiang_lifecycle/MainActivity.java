851d91f6dd5ada324e7ccc0557b82b4eb0f26e14
truonggiang76543 <63271620+truonggiang76543@users.noreply.github.com>
2020-04-22 21:09:21 +0700

    giang

--- app/src/main/java/vn/edu/ntu/nguyentruonggiang/nguyentruonggiang_lifecycle/MainActivity.java
+++ app/src/main/java/vn/edu/ntu/nguyentruonggiang/nguyentruonggiang_lifecycle/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("LifeCycle", "onCreate");
 
+
         Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_SHORT).show();
     }
 
